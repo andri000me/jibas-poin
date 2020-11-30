b963b8e77f04b4317feaf54353e5da0175012f4c
Dede Iskandar <dd@smkplusalfarhan.sch.id>
2020-11-30 11:09:29 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -41,7 +41,7 @@ date_default_timezone_set("Asia/Jakarta");
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/poin';
+$config['base_url'] = 'http://192.168.100.101/poin';
 
 /*
 |--------------------------------------------------------------------------
